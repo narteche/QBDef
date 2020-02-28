6261a6bfec875464b2075c5a677a958a72782c74
Noel Arteche <noel.arteche@gmail.com>
2020-02-28 19:33:50 +0100

    Internal representation of formulas completed - working on the parsing-to-representation transformation

--- src/parser.py
+++ src/parser.py
@@ -6,7 +6,6 @@ Created on Thu Nov 14 12:38:01 2019
 @author: alephnoell
 """
 
-
 from lark import Lark, Transformer, v_args
 from representation import QBF
 
@@ -25,15 +24,15 @@ class TraverseTree(Transformer):
         self.formula = QBF()
     
     def handle_value(self, name, expr):
-        print("VALUE: Handling parameter {} with value {}".format(name, expr))
-        self.formula.set_value(str(name), str(expr))
+        print("VALUE: Handling parameter {} with value {}.".format(name, expr))
+        self.formula.add_value(str(name), str(expr))
 
     def set_name(self, name):
-        print("NAME: setting name {}".format(name))
+        print("NAME: setting name \"{}\".".format(name))
         self.formula.set_name(str(name))
         
     def set_format(self, f):
-        print("FORMAT: setting format {}".format(f))
+        print("FORMAT: setting format \'{}\'.".format(f))
         self.formula.set_format(str(f))
         
     def add_parameter(self, p, t, *c):
@@ -67,57 +66,107 @@ class TraverseTree(Transformer):
                
                 completeRange = [theIndices, [lim1, lim2]]
                 completeRanges.append(completeRange)
-                
-        print("VARIABLE: adding variable {} with indices {} and ranges {}".format(varName, varIndices, completeRanges))
         self.formula.add_variable(varName, varIndices, completeRanges)
+        print("VARIABLE: adding variable {} with indices {} and ranges {}".format(varName, varIndices, completeRanges))
+
+    def add_block(self, block_def):
+        to_send = self.handle_block_def(block_def)
+        self.formula.add_block(to_send[0], to_send[1])
         
-    def return_formula(self):
+    def add_blocks(self, *everything):
+        grouping = []
+        definitions = []
+        conditions = []
+        
+        for elem in everything:
+            grps = elem.find_data("grouping")
+            for g in grps:
+                grouping = g
+            
+            defs = elem.find_data("single_block_def")
+            for d in defs:
+                definitions.append(d)
+                     
+            conds = elem.find_data("conditions")
+            for c in conds:
+                for cprime in c.children:
+                    conditions.append(cprime)
+        
+        grouping_to_send = self.handle_grouping(grouping)
+        defs_to_send = [self.handle_block_def(d) for d in definitions]
+        conds_to_send = [self.handle_condition(c) for c in conditions]
+        
+        self.formula.add_blocks(grouping_to_send, defs_to_send, conds_to_send)
+     
+    def handle_condition(self, condition):
+        condition = condition.children[0]
+        condition_to_send = []
+        if condition.data == 'index_range':
+            condition_to_send = ['range', [str(ix) for ix in condition.children[0].children], str(condition.children[1]), str(condition.children[2])]
+        elif condition.data == 'assignment':
+            condition_to_send = ['assignment', str(condition.children[0]), str(condition.children[1])]
+        else:
+            condition_to_send = ['other', str(condition.children[0])]
+            
+        return condition_to_send
+        
+        
+    def handle_block_def(self, block_def):
+        block_name = str(block_def.children[0])
+        indices = []
+        if block_def.children[1].data == "indices":
+            for ix in block_def.children[1].children:
+                indices.append(str(ix))
+        
+        block_name = (block_name, indices)
+
+        bricks = block_def.find_data("brick")
+        bricks_to_send = []
+        for b in bricks:
+            components = b.children
+            brick_to_send = []
+            if components[0] == "all blocks in":
+                brick_to_send = ["all blocks in", str(components[1])]                
+            else:    
+                sign = ''
+                if components[0] == '-':
+                    sign = '-'
+                    components = components[1:]
+                
+                name = str(components[0])
+                indices = []
+                if len(components) == 2:
+                    for ix in components[1].children:
+                        indices.append(str(ix))
+                    
+                brick_to_send = [(sign, name), indices]
+            bricks_to_send.append(brick_to_send)
+        
+        print("BLOCK: adding block {} with body {}.".format(block_name, bricks_to_send))
+        return block_name, bricks_to_send
+    
+    def handle_grouping(self, grp):
+        return str(grp.children[0])
+            
+    def return_formula(self, *r):
         return self.formula
         
 grammar_file = open("grammar.lark", "r")
 grammar = grammar_file.read()
 parser = Lark(grammar, parser='lalr', transformer=TraverseTree())
 parse = parser.parse
-
-#def main():
-#    while True:
-#        try:
-#            s = input('> ')
-#        except EOFError:
-#            break
-#        c = parse(s)
-#        
-#        for sub in c.iter_subtrees():
-#            if sub.data == "formula_family":
-#                chil = sub.children
-#                o = chil[len(chil) - 1]
-#                print(o)
-#                name = o.get_name()
-#                print(name)
-#                f = o.get_format()
-#                print(str(f))
-#     
+   
 
 def main():
     f = open("ch_test.txt", "r")
     s = f.read()
     parsed_formula = parse(s)
-    print(parsed_formula.pretty())
+    #print(parsed_formula.pretty())
     formula = None
     for sub in parsed_formula.iter_subtrees():
         if sub.data == "formula_family":
             chil = sub.children
             formula = chil[len(chil) - 1]
-
-    #print(formula)
-    #varabs = formula.get_variables()
-    #print(varabs)
-
-
-#def test():
-#    print(calc("a = 1+2"))
-#    print(calc("1+a*-3"))
-
 
 if __name__ == '__main__':
     # test()
--- src/representation.py
+++ src/representation.py
@@ -5,12 +5,13 @@ Created on Thu Nov 14 11:54:40 2019
 
 @author: alephnoell
 """
+
 from enum import Enum
 
 class Quantifier(Enum):
     EXISTS = 'E'
     FORALL = 'A'
-
+    
 class Operator(Enum):
     OR = 'or'
     AND = 'and'
@@ -20,50 +21,51 @@ class Format(Enum):
     circuit = 'circuit'
     CNF = 'CNF'
     
-class ParamType(Enum):
-    natural = 'natural'
-    integer = 'int'
-    string = 'str'
-    boolean = 'bool'
-    alist = 'list'
-    aSet = 'set'
-    
-    
+  
 class Parameter:
     
-    def __init__(self, pName = "", pType=ParamType.natural, pValue = None, pRest = []):
+    def __init__(self, pName = "", pType="", pValue = None, pCons = [], pEval = []):
         self.paramName = pName
         self.paramType = pType
         self.paramValue = pValue
-        self.paramRestrictions = pRest
+        self.paramConstraints = pCons
+        self.paramEvaluatedConstraints = pEval
         
-    def get_name(self):
-        return self.paramName
+
+class Block:
     
-class Variable:
-    def __init__(self, pName = "", pType=ParamType.natural, pValue = None, pRest = []):
-        self.paramName = pName
-        self.paramType = pType
-        self.paramValue = pValue
-        self.paramRestrictions = pRest
-    
+    def __init__(self, bName = "", bId = "", bBody = [], bGroup = None, bAtt = []):
+        self.blockName = bName
+        self.blockId = bId
+        self.blockBody = bBody
+        self.blockGroup = bGroup
+        self.blockAtt = bAtt
+
 class QBF:
     def __init__(self):
-        self.varCounter = 0
+
+        self.values = {}
         
         self.name = ""
-        self.format = ""
-        self.values = {}
-        self.parameters = []
-        self.readVariables = []
-        self.variables = {}
+        self.format = None
         
+
+        self.parameters = []
+        
+        self.idCounter = 0
+        self.variables = {}
+        self.blocks = {}
+        self.block_contents = {}
+        self.groupings = {}
+    
+    # ====== Name ======
     def get_name(self):
         return self.name
     
     def set_name(self, n):
         self.name = n
         
+    # ===== Format ======
     def get_format(self):
         return self.format
     
@@ -73,6 +75,7 @@ class QBF:
         else:
             self.format = Format.circuit
             
+    # ====== Values ======
     def get_values(self):
         return self.values
     
@@ -82,56 +85,70 @@ class QBF:
     def get_value(self, name):
         return self.values[name]
     
-    def set_value(self, name, expression):
-        self.values[name] = eval(expression)
+    def add_value(self, name, expression):
+        self.values[name] = self.evaluate(expression)
     
+    # ====== Parameters ======
     def get_parameters(self):
         return self.parameters
     
-    def get_parameter(self, pName):
-        params = self.parameters
-        for p in params:
-            if p.get_name() == pName:
-                return p
-            
+    def set_parameters(self, params):
+        self.parameters = params
+    
     def add_parameter(self, paramName, paramType, cons):
-        value = self.values[paramName]
+        try:
+            value = self.values[paramName]
+        except:
+            print("PARAMETER ERROR: Cannot find appropriate value for parameter {}".format(paramName))
+            exit()
+        
         constraints = []
         for expr in cons:
-            #res = eval(expr.replace(paramName, str(value)))
             res = self.evaluate(expr) # bring variables into scope
             constraints.append(res)
-        self.parameters.append(Parameter(paramName, paramType, value, constraints))
+        for i in range(len(constraints)):
+            if not constraints[i]:
+                print("PARAMETER ERROR: Constraint \'{}\' for parameter {} was violated.".format(cons[i], paramName))
+        self.parameters.append(Parameter(paramName, paramType, value, cons, constraints))
         
-    def add_variable(self, varName, varIndices=[], varRanges=[]):
-        print(varIndices)
-        if varIndices and varRanges:
-            self.recursive_nesting(varRanges, 0, 0, varName, [])
-        elif varIndices:
-            varName = varName + "("
-            for inx in varIndices:
-                varName = varName + ' ' + str(inx)
-            varName = varName + ')'
-            self.varCounter = self.varCounter + 1
-            varId = self.varCounter
-            self.variables[varName] = varId
-        else:
-            self.varCounter = self.varCounter + 1
-            varId = self.varCounter
-            self.variables[varName + "( )"] = varId
-            
+        
+    # ====== Variables ======
     def get_variables(self):
         return self.variables
+    
+    def set_variables(self, newVars):
+        self.variables = newVars
+        
+    def get_variable_id(self, normVarName):
+        try:
+            return self.variables[normVarName]
+        except:
+            print("VARIABLE ERROR: Variable {} has not been declared.".format(normVarName))
+
+    def add_variable(self, varName, varIndices=[], varRanges=[]):
+        if varIndices and varRanges:
+            self.recursive_nesting(varRanges, 0, 0, varName, [])
+        else:
+            self.save_variable(self.normalize_name(varName, varIndices))
+
+    def save_variable(self, normVarName):
+        if normVarName in self.variables:
+            print("VARIABLE ERROR: Variable {} is being declared more than once!".format(normVarName))
+        else:
+            self.idCounter = self.idCounter + 1
+            self.variables[normVarName] = self.idCounter
+
+            
+    def normalize_name(self, varName, varIndices):
+        varName = varName + '('
+        for inx in varIndices:
+            varName = varName + ' ' + str(inx)
+        varName = varName + ' )'
+        return varName
         
     def recursive_nesting(self, levels, currentLevel, currentIndex, varName, valuedIndices):
         if currentLevel == len(levels):
-            name = varName + "("
-            for inx in valuedIndices:
-                name = name + ' ' + str(inx)
-            name = name + ' )'
-            self.varCounter = self.varCounter + 1
-            varId = self.varCounter
-            self.variables[name] = varId
+            self.save_variable(self.normalize_name(varName, valuedIndices))
             return
             
         level = levels[currentLevel]
@@ -149,6 +166,84 @@ class QBF:
             vals.append(index)
             self.recursive_nesting(levels, currentLevel, currentIndex + 1, varName, vals)
             
+    # ====== Blocks ======
+    def add_blocks(self, grouping, defs, conditions):
+        self.process_conditions(conditions, 0, {}, defs, 0, grouping)        
+        
+    def process_conditions(self, conditions, currentCondition, valuedIndices, block_defs, currentIndex, grouping):
+        if currentCondition == len(conditions):
+            for definition in block_defs:
+                self.add_block(definition[0], definition[1], grouping, valuedIndices)
+            return
+            
+        cond = conditions[currentCondition]
+        if cond[0] == 'range':
+            indices = cond[1]
+            if currentIndex == len(indices):
+                self.process_conditions(conditions, currentCondition + 1, valuedIndices, block_defs, 0, grouping)
+            else:
+                lim1 = self.evaluate(cond[2])
+                lim2 = self.evaluate(cond[3])
+                for indexVal in range(lim1, lim2 + 1):
+                    valuedIndices[indices[currentIndex]] = indexVal
+                    self.process_conditions(conditions, currentCondition, valuedIndices, block_defs, currentIndex + 1, grouping)
+        elif cond[0] == 'assignment':
+            valuedIndices[cond[1]] = self.evaluate(cond[2])
+            self.process_conditions(conditions, currentCondition + 1, valuedIndices, block_defs, currentIndex, grouping)
+        else:
+            if not self.evaluate(cond[1]):
+                return
+            else:
+                self.process_conditions(conditions, currentCondition + 1, valuedIndices, block_defs, currentIndex, grouping)
+       
+    def substitute(self, indices, values):
+        subs = []
+        for ix in indices:
+            if ix in values:
+                subs.append(values[ix])
+            else:
+                subs.append(ix)
+        return subs
+    
+    def add_block(self, name, bricks, grp = None, valuedIndices = {}):
+        indices = self.substitute(name[1], valuedIndices)
+        name = self.normalize_name(name[0], indices)
+        self.save_block(name)
+
+        contents = []
+        for brick in bricks:
+            if brick[0] == "all blocks in":
+                print("Not ready")
+                #contents = contents + self.groupings[brick[1]]
+            else:
+                bSign = brick[0][0]
+                bName = brick[0][1]
+                indices = self.substitute(brick[1], valuedIndices)
+                brickId = self.get_brick_id(self.normalize_name(bName, indices))
+                contents.append(int(bSign + str(brickId)))
+        
+        self.save_block_contents(name, contents, grp)
+                
+    def get_brick_id(self, normName):
+        if normName in self.variables:
+            return self.variables[normName]
+        elif normName in self.blocks:
+            return self.blocks[normName]
+        else:
+            print("BRICK ERROR: Block or variable {} has not been declared.".format(normName))
+
+    def save_block(self, normBlockName):
+        if normBlockName in self.blocks:
+            print("BLOCK ERROR: Block {} is being declared more than once!".format(normBlockName))
+        else:
+            self.idCounter = self.idCounter + 1
+            self.blocks[normBlockName] = self.idCounter
+            
+    def save_block_contents(self, normName, contents, grp):
+        bId = self.get_brick_id(normName)
+        self.block_contents[bId] = Block(normName, contents, grp)
+            
+    # ====== EXTRA: expression evaluation ======
     def evaluate(self, expr):
         variables = [var for var in self.values]
         assigned_values = [val for val in self.values.values()]
@@ -156,7 +251,5 @@ class QBF:
         for p in pairs:
             assignment = "{} = {}".format(p[0], p[1])
             exec(assignment)
-        return eval(expr)    
+        return eval(expr)
         
-        
-    
